0a33a627412e17f61bb07f31cf7cc5624e4c36b9
ubuntu <itsarjunpaleri@gmail.com>
2018-11-13 16:16:49 +0530

    5th day

--- python/flaskpgms/app.py
+++ python/flaskpgms/app.py
@@ -1,17 +1,32 @@
-from flask import Flask
+from flask import Flask,render_template,request
 
 app=Flask(__name__)
 
-@app.route('/')
+
+@app.route('/send',methods=['GET','POST'])
+def send():
+    if(request.method=='POST'):
+        getname=request.form['name']
+        getemail=request.form['email']
+        getmobile=request.form['mobile']
+        getaddress=request.form['address']
+        getmessage=request.form['message']
+
+        return render_template('result.html',newname=getname,newemail=getemail,newmobile=getmobile,newaddress=getaddress,newmessage=getmessage)
+
+
+@app.route('/contact',)
 def h():
-    return "welcome to flask"
+    return render_template("contact.html")
 
 @app.route('/home')
 def home():
-    return "welcome to home"
+    return render_template("home.html")
+
 
 @app.route('/about')
 def about():
-    return "welcome to about"
+    return render_template("about.html")
+
 if(__name__=='__main__'):
     app.run(debug=True)
